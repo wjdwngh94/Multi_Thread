9b17c3e9b516ab56eff931312e8fa8b71d2bcb26
404-10 <404-10@404-10-PC>
2015-07-27 14:57:04 +0900

    thread _ time

--- src/thread/Multi_Thread_04.java
+++ src/thread/Multi_Thread_04.java
@@ -1,4 +1,11 @@
 package thread;
+
+import java.text.SimpleDateFormat;
+
+import javax.swing.JOptionPane;
+
+import java.util.Date;
+
 /*
  * 시계만들기
  * 
@@ -13,6 +20,39 @@ package thread;
  * 5초가 작동되도록 하시면 됩니다.
  * 
  */
-public class Multi_Thread_04 {
 
+class Timer extends Thread {
+	public void run() {
+		// 1초에 한번씩 시간을 출력
+
+		while (true) {
+
+			Date date = new Date();
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/"); 
+			// import java.text.SimpleDateFormat;
+
+			String today = sdf.format(date); // 내가 요청한 형식대로 값을 담는다
+
+			System.out.println(today);
+			try{
+				Thread.sleep(1000L); //1000L
+			}catch(InterruptedException e){}
+		}
+	}
+}
+
+public class Multi_Thread_04 {
+	public static void main(String[] args) {
+
+		//스레드 생성 및 구동
+		
+		Timer timer = new Timer();
+		timer.setDaemon(true);
+		timer.start();
+		
+		String text = JOptionPane.showInputDialog("입력");
+		System.out.println("입력된 메세지 : " + text);
+
+	}
 }
